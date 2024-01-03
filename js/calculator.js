ca24de89112f4296f087a3d9d609f8c7b1843b0b
SouSouza_ <suehmats@gmail.com>
2024-01-02 23:58:26 -0300

    Structural update of comments

--- js/calculator.js
+++ js/calculator.js
@@ -6,100 +6,111 @@ let calculatorArray =
     operator: ``,
   }
 
-// Display                                       // Mostrar
-function updateDisplay                           // Atualizar display
-  ()
-  {
-    window.document.getElementById(`display`).value = 
-    String(calculatorArray.display)
-  } updateDisplay()
+// Display (Mostrar) ------------------------------------------------------------------------------
+
+  // Atualizar display
+  function updateDisplay                       
+    () 
+    {
+      window.document.getElementById(`display`).value = 
+      String(calculatorArray.display)
+    } updateDisplay()
 
 
-// Buttons                                       // Botões
-function clearCalculatorMemory                   // Limpar Memória da Calculadora
-  ()
-  {
-    calculatorArray.display = ``
-    calculatorArray.insertDot = false
-    calculatorArray.memory = 0
-    calculatorArray.operator = ``
-    calculatorArray.temp = 0
-    updateDisplay()
-  }
+// Buttons (Botões)  ------------------------------------------------------------------------------
 
-function deleteLastEnteredCharacter              // Apagar último caractere digitado
-  ()
-  {
-    calculatorArray.display = calculatorArray.display.slice(0,-1)
-    updateDisplay()
-  }
-
-function insertCharacter                         // Inserir caractere
-  (character)
-  {
-    if (character == `.`)
-      {
-        decimalPointCheck()
-      }
-    
-    if (typeof(character) == `number` || !calculatorArray.insertDot)
-      {
-        calculatorArray.display = String(calculatorArray.display) + character
-        updateDisplay()
-      }
-  }
-
-function selectOperator                          // Selecionar Operador
-  (operator)
-  {
-    calculate()
-    calculatorArray.operator = operator
-    calculatorArray.display = ``
-  }
-
-function calculationResult                     // Resultado do Calculo
-  ()
-  { 
-    calculate()
-    calculatorArray.display = ``
-  }
-
-// Special Functions                             // Funções Especiais
-function decimalPointCheck
-  ()
-  {
-    if (calculatorArray.display.length != calculatorArray.display.replace(`.`,``).length)
-      {
-        calculatorArray.insertDot = true
-      } else {
-        calculatorArray.insertDot = false
-      }
-  }
-
-function calculate                               // Calcular
-  ()
-  {
-    if (calculatorArray.memory == 0)
-      {
-        calculatorArray.memory = Number(calculatorArray.display)
-      } else {
-        switch (calculatorArray.operator)
-          {
-            case `+`: 
-              calculatorArray.memory = Number(calculatorArray.memory) + Number(calculatorArray.display)
-              break
-            case `-`:
-              calculatorArray.memory = calculatorArray.memory - calculatorArray.display
-              break
-            case `*`:
-              calculatorArray.memory = calculatorArray.memory * calculatorArray.display
-              break
-            case `/`:
-              calculatorArray.memory = calculatorArray.memory / calculatorArray.display
-              break
-          }
-      }
-      calculatorArray.display = calculatorArray.memory
+  // Limpar Memória da Calculadora
+  function clearCalculatorMemory                   
+    ()
+    {
+      calculatorArray.display = ``
+      calculatorArray.insertDot = false
+      calculatorArray.memory = 0
+      calculatorArray.operator = ``
+      calculatorArray.temp = 0
       updateDisplay()
-  }
+    }
+
+  // Apagar último caractere digitado
+  function deleteLastEnteredCharacter              
+    () 
+    {
+      calculatorArray.display = calculatorArray.display.slice(0,-1)
+      updateDisplay()
+    }
+
+  // Inserir caractere
+  function insertCharacter                         
+    (character) 
+    {
+      if (character == `.`)
+        {
+          decimalPointCheck()
+        }
+      
+      if (typeof(character) == `number` || !calculatorArray.insertDot)
+        {
+          calculatorArray.display = String(calculatorArray.display) + character
+          updateDisplay()
+        }
+    }
+
+  // Selecionar Operador
+  function selectOperator                          
+    (operator) 
+    {
+      calculate()
+      calculatorArray.operator = operator
+      calculatorArray.display = ``
+    }
+
+  // Resultado do Calculo
+  function calculationResult                     
+    () 
+    { 
+      calculate()
+      calculatorArray.display = ``
+    }
+
+// Special Functions (Funções Especiais)  ---------------------------------------------------------                       
+
+  // Verificação de ponto decimal
+  function decimalPointCheck
+    () 
+    {
+      if (calculatorArray.display.length != calculatorArray.display.replace(`.`,``).length)
+        {
+          calculatorArray.insertDot = true
+        } else {
+          calculatorArray.insertDot = false
+        }
+    }
+
+  // Calcular
+  function calculate                               
+    () 
+    {
+      if (calculatorArray.memory == 0)
+        {
+          calculatorArray.memory = Number(calculatorArray.display)
+        } else {
+          switch (calculatorArray.operator)
+            {
+              case `+`: 
+                calculatorArray.memory = Number(calculatorArray.memory) + Number(calculatorArray.display)
+                break
+              case `-`:
+                calculatorArray.memory = calculatorArray.memory - calculatorArray.display
+                break
+              case `*`:
+                calculatorArray.memory = calculatorArray.memory * calculatorArray.display
+                break
+              case `/`:
+                calculatorArray.memory = calculatorArray.memory / calculatorArray.display
+                break
+            }
+        }
+        calculatorArray.display = calculatorArray.memory
+        updateDisplay()
+    }
 
